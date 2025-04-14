fa6d6ade0384e9d69417bf5a860841bc08188cd0
Allen Christian Custodio <allenchristiancustodio@gmail.com>
2025-04-14 05:11:30 -0400

    Corrected import path for auth store in EmailVerificationPage component.

--- client/src/pages/EmailVerificationPage.tsx
+++ client/src/pages/EmailVerificationPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuthStore } from "../stores/authstore";
+import { useAuthStore } from "../stores/authStore";
 import toast from "react-hot-toast";
 import { motion } from "framer-motion";
 
